7757ca59c97cc8be838009a746424283eae7b633
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-05-27 00:53:26 +0300

    created partials

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
-
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 
 // express static bir middleware(ara yazılım) fonksiyonudur.
 // req-res döngüsü içindeki her şey middleware'dir.
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-
-
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
-})
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
 
 const port = 3000;
 app.listen(port, () => {
