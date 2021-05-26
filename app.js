f8d88160b211dc2162a98cb43ec0f396d0d7df01
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-05-26 23:30:16 +0300

    Static Files Uploaded

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+
+//MIDDLEWARES
+
+// express static bir middleware(ara yazılım) fonksiyonudur.
+// req-res döngüsü içindeki her şey middleware'dir.
+
+app.use(express.static('public'))
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
