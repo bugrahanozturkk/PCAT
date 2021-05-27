2c2e518707bcfb7f27b3e4a48bea9eb5f1eb91b1
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-05-27 19:10:37 +0300

    created database

--- models/Photo.js
+++ models/Photo.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //create schema
+
 const PhotoSchema = new Schema({
   title: String,
   description: String,
-  image: String,
+  Image: String,
   dateCreated: {
     type: Date,
     default: Date.now,
